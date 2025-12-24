f45f17ff7f2f6e07de9a1a5ba62117e5b59c7ce6
CarlosLC-hub <lopezcolome@gmail.com>
2025-12-23 22:09:39 -0600

    Add files via upload

---
+++ TriviaTJ/service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "trivia-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/game.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
