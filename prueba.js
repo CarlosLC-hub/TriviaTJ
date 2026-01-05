c63ba8a72cf4eeab1a792b8c4d3fa9373ab82a9c
CarlosLC-hub <lopezcolome@gmail.com>
2026-01-04 21:28:16 -0600

    Delete prueba.js

--- prueba.js
+++
@@ -1,9 +0,0 @@
-const prueba = [
-
-  /*1*/ { q: "¿Cómo me llamo?", a: ["Carlos","66","69"], c: 0,},
-  /*1*/ { q: "¿Cómo me llamo?", a: ["Carlos","66","69"], c: 0,},
-  /*1*/ { q: "¿Cómo me llamo?", a: ["Carlos","66","69"], c: 0,},
-  /*1*/ { q: "¿Cómo me llamo?", a: ["Carlos","66","69"], c: 0 },
-  /*1*/ { q: "¿Cómo me llamo?", a: ["Carlos","66","69"], c: 0 },
-
-]
